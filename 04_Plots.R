405a8e7e4eab5d2d243a015be999518981cabdc1
TrevorHD <44789861+TrevorHD@users.noreply.github.com>
2023-12-03 16:32:59 -0800

    Analysis update 37
    Added dynamic scaling for x-axis of spread GIFs, improved code annotation for the GIF plotting function, and updated the GIFs currently in the repo.

--- 04_Plots.R
+++ 04_Plots.R
@@ -2,32 +2,46 @@
 
 # Function to generate GIF of population spread (nYear = 100 recommended)
 generatePlots <- function(type = "hist"){
+  
+  # Set x-axis maximum based on wavefront at end of simulation
+  max_scale <- (ceiling(max(wv_plots[[length(wv_plots)]])/1000) + 1)*1000
+  
+  # Generate plot using position data at each timestep
   for(i in 1:length(wv_plots)){
+    
+    # Initialise vectors
     lower <- c()
-    positions <- unlist(wv_plots[i])
+    positions <- wv_plots[[i]]
     if(length(positions) == 0){
       positions <- 0}
+    
+    # Assume max density behind trimmed wavefront
+    # Trim condition here should match that used in the simulation
     if(max(positions > 1000)){
       minBin <- sum(positions > floor(min(positions)) & positions < ceiling(min(positions)))
       lower <- c(rep(0:(floor(min(positions) - 1)), 10) + 0.01,
                  rep(floor(min(positions)) + 0.01, 10 - minBin))
       if(min(positions) < 1){
         lower <- sort(lower)[-c(1:20)]}}
+    
+    # Plot wave movement as histogram
     if(type == "hist"){
-      hist(c(lower, positions), breaks = seq(0, 10000, by = 20), xlim = c(0, 10000), ylim = c(0, 250),
+      hist(c(lower, positions), breaks = seq(0, max_scale, by = 20), xlim = c(0, max_scale), ylim = c(0, 250),
            xaxt = "n", yaxt = "n", xlab = "Distance (m)", ylab = "Count", main = "")
-      axis(1, at = seq(0, 10000, 2000))
+      axis(1, at = seq(0, max_scale, 1000))
       axis(2, at = seq(0, 500, 50))
-      text(x = 9400, y = 230, paste0("t = ", i))
+      text(x = max_scale*0.95, y = 230, paste0("t = ", i))
       box()}
+    
+    # Plot wave movement as density relative to maximum
     if(type == "density"){
-      plotdata <- hist(c(lower, positions), breaks = seq(0, 10000, by = 20), plot = FALSE)
+      plotdata <- hist(c(lower, positions), breaks = seq(0, max_scale, by = 20), plot = FALSE)
       plot(plotdata$mids, plotdata$density/max(plotdata$density),
-           type = "l", xlim = c(0, 10000), ylim = c(0, 1.25),
+           type = "l", xlim = c(0, max_scale), ylim = c(0, 1.25),
            xaxt = "n", yaxt = "n", xlab = "Distance (m)", ylab = "Relative Density", main = "")
-      axis(1, at = seq(0, 10000, 2000))
+      axis(1, at = seq(0, max_scale, 1000))
       axis(2, at = seq(0, 1.25, 0.25))
-      text(x = 9400, y = 1.15, paste0("t = ", i))}}}
+      text(x = max_scale*0.95, y = 1.15, paste0("t = ", i))}}}
 
 # Generate GIFs of population spread
 save_gif(generatePlots("hist"), "Spread1.gif", delay = 0.2, width = 1280, height = 720, res = 144)
