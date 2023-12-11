23411f311c9845277cb7a2cc68bacd7e7b91087d
TrevorHD <44789861+TrevorHD@users.noreply.github.com>
2023-12-10 21:40:44 -0800

    Analysis update 39
    Added code for plotting parameter elasticity, as well as a placeholder figure until all of the simulations have been run.

--- 04_Plots.R
+++ 04_Plots.R
@@ -115,3 +115,47 @@ dev.off()
 save_gif(generatePlots("hist"), "Spread1.gif", delay = 0.2, width = 1280, height = 720, res = 144)
 save_gif(generatePlots("density"), "Spread2.gif", delay = 0.2, width = 1280, height = 720, res = 144)
 
+
+
+
+
+##### Create function to plot parameter elasticity --------------------------------------------------------
+
+# Create placeholder data with elasticity for each scalable parameter
+temp <- data.frame(var = c("Vegetation height", "Mean wind speed", "SD wind speed",
+                           "Mean terminal velocity", "SD terminal velocity",
+                           "Intercept prob. ant dispersal", "Slope prob. ant dispersal",
+                           "Seeds per flower head", "Prob. estab. from seed",
+                           "Prob. seed predation", "Prob. seed entering SB",
+                           "Prob. estab. from SB", "Prob. survival in SB",
+                           "Mean rosette size", "SD rosette size", "Prob. flowering",
+                           "Intercept num. heads", "Slope num. heads", "Mean head height",
+                           "SD head height", "Prob. survival"),
+                   vals = seq(-1, 1, length.out = 21))
+
+# Prepare graphics device
+tiff(filename = "Figure 2.tif", width = 2000, height = 2000, units = "px",
+     res = 800, compression = "lzw")
+
+# Create blank page
+grid.newpage()
+plot.new()
+
+# Set grid layout and activate it
+gly <- grid.layout(2000, 2000)
+pushViewport(viewport(layout = gly))
+
+# Plot elasticity values
+pushViewport(vp = viewport(layout.pos.row = 1:2000, layout.pos.col = 1:2000))
+par(fig = gridFIG())
+par(new = TRUE, mar = c(1.5, 3.7, 0.2, 0.5), cex = 0.4, cex.axis = 0.75)
+barplot(temp$vals, space = 0.7, horiz = TRUE, mgp = c(0.3, -0.25, 0),
+        xlab = "Elasticity", tcl = -0.15)
+axis(2, at = seq(1.25, 35.3, length.out = 21), labels = temp$var,
+     las = 2, mgp = c(0.85, 0.2, 0), tcl = 0)
+box()
+popViewport()
+
+# Deactivate grid layout; finalise graphics save
+dev.off()
+
